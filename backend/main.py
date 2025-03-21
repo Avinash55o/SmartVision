4d63f9d12dbc55dcd046e9d145a14bff886d27e0
Avinash Boruah <avinashboruah8@gmail.com>
2025-03-21 19:30:41 +0530

    add blip and gemma

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,21 @@
-# main.py
-
-from fastapi import FastAPI
-
+from fastapi import FastAPI,File,UploadFile
+from fastapi.responses import JSONResponse
+from PIL import Image
+import torch
+import clip
+import openvino.runtime as ov
+import io
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Zero-Shot Image Classifier Backend is running!"}
+device="cuda" if torch.cuda.is_available() else "cpu"
+clip_model, preprocess=clip.load("ViT-B/32",device=device)
+
+@app.post("/generate-caption/")
+async def generate_caption(file:uploadFile =File(...)):
+    try:
+        image=Image.open(io.BytesIO(await file.read()))
+        image_input=preprocess(image).unsqueeze(0).to(device)
+
+
+        with torch.no_grand():
+            image_features= clip_model.encode_image(image_input).float()
